c6bfde6c76a2c659926bc7f493a689fa6d1d5c74
cztamas <tamas.czank@gmail.com>
2021-05-13 20:07:03 +0200

    play around with react a very little bit

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,6 +2,29 @@
 
 const React = require('react');
 
-module.exports = function App() {
-  return <h1>Hello, world!</h1>;
+const startPosition = [5, 3, 8, 12];
+const range = length => Array(length).fill('').map((_, index) => index + 1);
+
+module.exports = class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      gameState: startPosition
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        {
+          this.state.gameState.map((itemsInRow, rowIndex) => (
+            <div key={ rowIndex }>{
+              range(itemsInRow).map(columnIndex => <div className="cell" key={ columnIndex } />)
+            }</div>
+          ))
+        }
+      </div>
+    );
+  }
 };
