87a8cf80eaaf6d09dfadaeefb939c3615b82db6c
cztamas <tamas.czank@gmail.com>
2021-05-24 17:37:44 +0200

    remove window.alert from useless game (because google sites blocks it anyway)

--- src/useless-example-game/useless-example-game.jsx
+++ src/useless-example-game/useless-example-game.jsx
@@ -4,16 +4,47 @@ const React = require('react');
 const { Link } = require('react-router-dom');
 
 module.exports = class UselessExampleGame extends React.Component {
+  constructor() {
+    super();
+    this.clear = this.clear.bind(this);
+    this.state = {
+      result: null
+    };
+  }
+
+  buttons() {
+    return (
+      <div>
+        <button onClick={ () => this.endGame(true) }>Nyerni szeretnék</button>
+        <button onClick={ () => this.endGame(false) }>Veszíteni szeretnék</button>
+      </div>
+    );
+  }
+
+  result() {
+    return (
+      <div>
+        <h2>{ this.state.result === 'win' ? 'Gratulálunk! Nyertél!' : 'Vesztettél :(' }</h2>
+        <button onClick={ this.clear }>Új játék</button>
+      </div>
+    );
+  }
+
+  clear() {
+    this.setState({ result: null });
+  }
+
   endGame(didPlayerWin) {
-    const message = didPlayerWin ? 'Gratulálunk! Nyertél!' : 'Vesztettél :(';
-    window.alert(message);
+    this.setState({
+      result: didPlayerWin ? 'win' : 'lose'
+    });
   }
 
   render() {
     return <div>
       <h1>Teljesen haszontalan játék</h1>
-      <button onClick={ () => this.endGame(true) }>Nyerni szeretnék</button>
-      <button onClick={ () => this.endGame(false) }>Veszíteni szeretnék</button>
+      { this.state.result ? this.result() : this.buttons() }
+
       <Link to="/">Vissza a főoldalra</Link>
     </div>;
   }
