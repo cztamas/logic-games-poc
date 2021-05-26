aad168efbf14664515056ac2fd417989626a6dd8
cztamas <tamas.czank@gmail.com>
2021-05-26 18:28:49 +0200

    refactor route handling

--- src/app.jsx
+++ src/app.jsx
@@ -1,35 +1,16 @@
 'use strict';
 
 const React = require('react');
-const { Switch, Route, Link } = require('react-router-dom');
+const { useRoutes } = require('react-router-dom');
 
+const Overview = require('./overview');
 const NimWithTransfer = require('./nim-with-transfer/nim-with-transfer');
 const UselessExampleGame = require('./useless-example-game/useless-example-game');
 
-module.exports = class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Switch>
-          <Route path="/strange">
-            <NimWithTransfer />
-          </Route>
-          <Route path="/useless">
-            <UselessExampleGame />
-          </Route>
-          <Route path="/">
-            <Overview />
-          </Route>
-        </Switch>
-      </div>
-    );
-  }
-};
+const routes = [
+  { path: '/', element: <Overview /> },
+  { path: '/strange', element: <NimWithTransfer /> },
+  { path: '/useless', element: <UselessExampleGame /> },
+];
 
-const Overview = () => (
-  <div>
-    <h1>Válassz egy játékot!</h1>
-    <Link to="/strange">Furcsa játék</Link><br />
-    <Link to="/useless">Ostoba játék</Link>
-  </div>
-);
+module.exports = () => useRoutes(routes);
---
+++ src/overview.jsx
@@ -0,0 +1,14 @@
+'use strict';
+
+const React = require('react');
+const { Link } = require('react-router-dom');
+
+module.exports = function Overview() {
+  return (
+    <div>
+      <h1>Válassz egy játékot!</h1>
+      <Link to="/strange">Furcsa játék</Link><br />
+      <Link to="/useless">Ostoba játék</Link>
+    </div>
+  );
+};
